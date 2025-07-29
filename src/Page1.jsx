d4b00fe5c361b3b01de9b013eca397a6262e5ca9
2400033243 <akshayaannapaneni1994@gmail.com>
2025-07-29 15:17:49 +0530

    version1

---
+++ src/Page1.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class Page1 extends Component {
+    render() {
+        return (
+            <div>
+                <h1>India is my Country</h1>
+                <br /><br />
+                <h2>Today's Topic is Introduction to css
+                </h2>
+            </div>
+        )
+    }
+}
